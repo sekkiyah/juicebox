c397cef7ff2d9c81ec8491edb8d9f203deac33ef
sekkiyah <seth_hinshaw@hotmail.com>
2022-09-21 00:43:21 -0500

    added db testing queries

--- db/seed.js
+++ db/seed.js
@@ -1,17 +1,88 @@
-const { client } = require('./index');
+const { client, getAllUsers, createUser } = require('./index');
 
-async function testDB() {
+async function dropTables() {
+  try {
+    console.log('Starting to drop tables...');
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `);
+    console.log('Finished dropping tables!');
+  } catch (error) {
+    console.error('Error dropping tables!');
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    console.log('Starting to build tables...');
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+      );
+    `);
+    console.log('Finished building tables!');
+  } catch (error) {
+    console.error('Error building tables!');
+    throw error;
+  }
+}
+
+async function createInitialUsers() {
+  try {
+    console.log('Starting to create users...');
+
+    await createUser({
+      username: 'albert',
+      password: 'bertie99',
+    });
+    await createUser({
+      username: 'sandra',
+      password: '2sandy4me',
+    });
+    await createUser({
+      username: 'glamgal',
+      password: 'soglam',
+    });
+
+    console.log('Finished creating users!');
+  } catch (error) {
+    console.error('Error creating users!');
+    throw error;
+  }
+}
+
+async function rebuildDB() {
   try {
     client.connect();
 
-    const result = await client.query(`SELECT * FROM users;`);
-
-    console.log(result);
+    await dropTables();
+    await createTables();
+    await createInitialUsers();
   } catch (error) {
+    console.error(error);
+  }
+}
+
+async function testDB() {
+  try {
+    console.log('Starting to test database...');
+
+    const users = await getAllUsers();
+    console.log('getAllUsers:', users);
+
+    console.log('Finished database tests!');
+  } catch (error) {
+    console.error('Error testing database!');
     console.error(error);
   } finally {
     client.end();
   }
 }
 
-testDB();
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
