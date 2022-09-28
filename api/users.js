e29dae9dd3809690c3ca5cbbff9afbd9333a9c03
sekkiyah <seth_hinshaw@hotmail.com>
2022-09-27 23:54:35 -0500

    added user registration endpoint

--- api/users.js
+++ api/users.js
@@ -8,7 +8,7 @@ usersRouter.use((req, res, next) => {
   next();
 });
 
-const { getAllUsers, getUserByUsername } = require('../db');
+const { getAllUsers, getUserByUsername, createUser } = require('../db');
 
 usersRouter.get('/', async (req, res) => {
   const users = await getAllUsers();
@@ -32,11 +32,8 @@ usersRouter.post('/login', async (req, res, next) => {
     const user = await getUserByUsername(username);
 
     if (user && user.password == password) {
-      const token = jwt.sign(
-        { id: user.id, username: username },
-        process.env.JWT_SECRET
-      );
-      res.send({ message: "you're logged in!", token: token });
+      const token = jwt.sign({ id: user.id, username }, process.env.JWT_SECRET);
+      res.send({ message: "you're logged in!", token });
     } else {
       next({
         name: 'IncorrectCredentialsError',
@@ -46,6 +43,46 @@ usersRouter.post('/login', async (req, res, next) => {
   } catch (error) {
     console.log(error);
     next(error);
+  }
+});
+
+usersRouter.post('/register', async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: 'UserExistsError',
+        message: 'A user by that username already exists',
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '1w',
+      }
+    );
+
+    res.send({
+      message: 'thank you for signing up',
+      token,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 });
 
